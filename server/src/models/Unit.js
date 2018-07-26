31521d6944e8241f254804cdd40cb369de07863c
Xiaoyan Liao <running923@gmail.com>
2018-07-26 13:10:27 -0700

    feature: add Unit model

---
+++ server/src/models/Unit.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const UnitSchema = new Schema({
+  name: {
+    type: String,
+    trim: true
+  },
+  icon: {
+    type: String,
+    default: "airplane.png",
+    trim: true
+  },
+  color: {
+    type: String,
+    default: "white",
+    trim: true
+  },
+  affiliation: {
+    type: String,
+    default: "",
+    trim: true
+  },
+  mechs: [{
+    type: Schema.Types.ObjectId,
+    ref: 'Pilot'
+  }]
+});
+
+module.exports = mongoose.model('Unit', UnitSchema);
