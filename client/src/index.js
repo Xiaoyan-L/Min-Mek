6040fad4db7d5d16800db14318cbfc4273867fa0
Xiaoyan Liao <running923@gmail.com>
2018-08-01 10:50:36 -0700

    chore: starter code

---
+++ client/src/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+//import "react-treeview/react-treeview.css"
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+import { Provider } from "react-redux";
+import store from "./store";
+import { BrowserRouter } from "react-router-dom";
+
+ReactDOM.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById("root")
+);
+registerServiceWorker();
